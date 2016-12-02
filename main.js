5050b09980ac7281cd6d4af6acfc3b49ac85dbe2
Christopher McNeal <camcneal@crimson.ua.edu>
2016-12-02 13:06:43 -0600

    everthing working properly

---
+++ main.js
@@ -0,0 +1,34 @@
+// console.log(markets[0].marketname);
+
+//pulls classes from the dom
+var marketList = document.querySelector('.marketList');
+var catList = document.querySelector('.catList');
+
+
+//create function that takes all markets lass than ten miles from the user out of the array
+var nearMark = markets.filter(function(market, i, arr) {
+    if (market.marketname.split(' ')[0] <= 10) {
+        return true;
+    }
+// make another function that shows the array and shows only the market name
+}).map(function(market, i, arr) {
+    return market.marketname.split(' ').slice(1).join(' ');
+});
+// console.log(nearMark);
+// go through array and append the markets that apply to the page
+nearMark.forEach(function(market, i) {
+    var li = $('<li>' + nearMark[i] + '</li>');
+    $('.marketList').append(li);
+})
+
+//
+var catPic = catGif.data.map(function(cat, i) {
+    // console.log(catGif.data[i].images.downsized.url);
+
+    return catGif.data[i].images.downsized.url;
+});
+
+catPic.forEach(function(cat, i) {
+    var img = $('<img src=' + catPic[i] + '>');
+    $('.catList').append(img);
+})
